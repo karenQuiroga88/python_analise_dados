f3a32ecf77fc727dbce081f2bd3c9daf641a425f
karenQuiroga88 <115047409+karenQuiroga88@users.noreply.github.com>
2025-09-20 17:35:32 -0300

    Create 06_confy.py

---
+++ 06_confy.py
@@ -0,0 +1,16 @@
+
+FOLDER = "C:/Users/sabado/Desktop/python_analise_dados/python_analise_dados-main/AIS/"
+DB_PATH = "bancoDeDadosAIS.db"
+FLASK_DEBUG = True
+FLASK_HOST = '127.0.0.1'
+FLASK_PORT = 5000
+
+# Rotas comuns do sistema
+ROTAS = [
+    "/",                        #rota00
+    "/upload",                  #rota01
+    "/consultar" ,               #rota02
+    "/graficos",                #rota03
+    "/editar_inadimplencia",     #rota04
+    "/correlacao"                #rota05
+]
